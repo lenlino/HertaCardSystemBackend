01cc80451a478c3065daeb1ad14a40ab9cc89207
lenlino <lenlino@hotmail.com>
2023-11-16 13:37:01 +0900

    rank

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import io
 
+import i18n
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from starlette.responses import StreamingResponse, Response
 
 import generate.generate
+from generate.utils import get_score_rank
 
 app = FastAPI()
 
@@ -18,4 +20,5 @@ async def gen_card(uid: str, select_number: int, is_uid_hide: bool, calculation_
     panel_img['img'].save(image_binary, 'PNG')
     image_binary.seek(0)
     print(image_binary)
-    return Response(content=image_binary.getvalue(), headers={"score": panel_img["score"]}, media_type="image/png")
+    score_rank = get_score_rank(int(panel_img['avatar_id']), uid, panel_img['score'])
+    return Response(content=image_binary.getvalue(), headers={"score": panel_img["score"], "score_rank": score_rank}, media_type="image/png")
