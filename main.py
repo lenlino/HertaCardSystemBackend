bf45b1a241e93bb55a14580448406a22e7d3afa2
lenlino <65199014+lenlino@users.noreply.github.com>
2023-12-24 18:16:58 +0900

    jsonのみの取り出し

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import i18n
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
-from starlette.responses import StreamingResponse, Response
+from starlette.responses import StreamingResponse, Response, JSONResponse
 
 import generate.generate
 from generate.utils import get_score_rank
@@ -16,15 +16,24 @@ app = FastAPI()
 
 
 @app.get("/gen_card/{uid}")
-async def gen_card(uid: str, select_number: int, is_uid_hide: bool, is_hide_roll: bool, calculation_value: str, lang: str):
+async def gen_card(uid: str, select_number: int, is_uid_hide: bool = False, is_hide_roll: bool = False,
+                   calculation_value: str = "compatibility", lang: str = "jp"):
     image_binary = io.BytesIO()
     panel_img = await generate.generate.generate_panel(uid=uid, chara_id=int(select_number), template=2,
                                                        is_hideUID=is_uid_hide
-                                                       , calculating_standard=calculation_value, lang=lang, is_hide_roll=is_hide_roll)
+                                                       , calculating_standard=calculation_value, lang=lang,
+                                                       is_hide_roll=is_hide_roll)
     panel_img['img'].save(image_binary, 'PNG')
     image_binary.seek(0)
     score_rank = get_score_rank(int(panel_img['avatar_id']), uid, panel_img['score'])
-    return Response(content=image_binary.getvalue(), headers={"X-score": str(panel_img["score"]), "X-top-score": score_rank['top_score'],
-                                                              'X-before-score': score_rank['before_score'], 'X-median': score_rank['median'],
-                                                              'X-mean': score_rank['mean'], 'X-rank': score_rank['rank'], 'X-data-count': 'data_count'},
+    return Response(content=image_binary.getvalue(),
+                    headers={"X-score": str(panel_img["score"]), "X-top-score": score_rank['top_score'],
+                             'X-before-score': score_rank['before_score'], 'X-median': score_rank['median'],
+                             'X-mean': score_rank['mean'], 'X-rank': score_rank['rank'], 'X-data-count': 'data_count'},
                     media_type="image/png")
+
+
+@app.get("/sr_info_parsed/{uid}")
+async def sr_info_parsed(uid: str, lang: str = "jp"):
+    json_compatible_item_data = jsonable_encoder(await generate.utils.get_json_from_url(uid, lang))
+    return JSONResponse(content=json_compatible_item_data)
