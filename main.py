62019234201351e2f67823730a2664f0c41ed794
lenlino <65199014+lenlino@users.noreply.github.com>
2023-12-28 10:30:40 +0900

    データ数ヘッダー
    Took 4 minutes

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ async def gen_card(uid: str, select_number: int, is_uid_hide: bool = False, is_h
     return Response(content=image_binary.getvalue(),
                     headers={"X-score": str(panel_img["score"]), "X-top-score": score_rank['top_score'],
                              'X-before-score': score_rank['before_score'], 'X-median': score_rank['median'],
-                             'X-mean': score_rank['mean'], 'X-rank': score_rank['rank'], 'X-data-count': 'data_count',
+                             'X-mean': score_rank['mean'], 'X-rank': score_rank['rank'], 'X-data-count': score_rank['data_count'],
                              'Access-Control-Allow-Origin': '*'},
                     media_type="image/png")
 
