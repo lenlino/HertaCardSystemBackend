fdd3c05fea8b61dbd737062fb1bce02f813b8828
lenlino <65199014+lenlino@users.noreply.github.com>
2025-02-05 20:29:00 +0900

    ✨ feat(generate/templates/two.py): フォールバック言語を英語に設定する機能を追加しました。

--- generate/templates/two.py
+++ generate/templates/two.py
@@ -14,6 +14,8 @@ from generate.utils import get_json_from_url, get_json_from_json, get_image_from
 
 font_file_path = f"{get_file_path()}/assets/zh-cn.ttf"
 
+i18n.set('fallback', 'en')
+
 
 async def generate_panel(uid="805477392", chara_id=1, is_hideUID=False, calculating_standard="compatibility",
                          lang="jp", is_hide_roll=False):
